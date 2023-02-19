c939ac66a0f836a284d91d0cb54e45fb4da7848e
Diego Ferruzzo <dferruzzo@gmail.com>
2023-02-19 20:03:50 -0300

    README file

--- main_modelo_nao_linear_v1.m
+++ main_modelo_nao_linear_v1.m
@@ -1,4 +1,4 @@
-% Arquivo principal da simulação
+% Arquivo principal da simulaï¿½ï¿½o
 % ------------------------------
 % Modelo 1
 % --------
@@ -13,7 +13,7 @@ Ixx = 1e-2;
 Iyy = 1e-2;
 Izz = 1.8e-2;
 Ir = 1e-3;
-% Transformação U1,...,U4 -> freq. angulares dos motores
+% Transformaï¿½ï¿½o U1,...,U4 -> freq. angulares dos motores
 kf = 1.4793*10^(-7); % constante de empuxo
 km = 2.4656*10^(-9); % constante de torque
 L = 0.2;
@@ -27,19 +27,19 @@ T_inv = T^(-1);
 Kp = 0.9182;    % ganho
 tau_a = 0.0569; % polo
 tau_s = 0.0452; % atraso de transporte
-% sem limitação dos motores por enquanto 08/11/2022
+% sem limitaï¿½ï¿½o dos motores por enquanto 08/11/2022
 %rpm_max = 15400;
 %rpm_min = 8700;
-% inlcuir dinâmica dos motores?
-% -1 NÃO
+% inlcuir dinï¿½mica dos motores?
+% -1 Nï¿½O
 % 1  SIM
-incluir_din_motor = -1;
-%% Condições iniciais
+incluir_din_motor = 1;
+%% Condiï¿½ï¿½es iniciais
 % CondiÃ§Ãµes iniciais Ã¢ngulos de Euler
 phi0 = 0;
 theta0 = 0;
 psi0 = 0;
-% CondiÃ§Ãµes iniciais derivada dos ângulos de Euler
+% CondiÃ§Ãµes iniciais derivada dos ï¿½ngulos de Euler
 dphi0 = 0;
 dtheta0 = 0;
 dpsi0 = 0;
@@ -56,11 +56,11 @@ z0 = 1;
 vxd = 0;
 vyd = 0;
 vzd = 0;
-%% controlador PID dinâmica translacional
+%% controlador PID dinï¿½mica translacional
 Kp_trans = 0.4;%0.5;
 Ki_trans = 0.06;
 Kd_trans = 0.002;
-%% Controlador PID para a dinâmica rotacional
+%% Controlador PID para a dinï¿½mica rotacional
 Kp_rot = 10;
 Ki_rot = 1.0;
 Kd_rot = 0.01;
